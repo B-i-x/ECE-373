5cc984a1fd255ed67de97555faaa4ce165f88101
B-i-x <bix.romero@gmail.com>
2023-08-29 08:37:48 -0700

    pre-lab1 done

---
+++ prelab1/src/person/main.java
@@ -0,0 +1,10 @@
+package person;
+// import java.util.*;
+
+public class main {
+    public static void main(String args[])
+        {
+        Person person=new Person();
+        person.display();
+        }
+}
