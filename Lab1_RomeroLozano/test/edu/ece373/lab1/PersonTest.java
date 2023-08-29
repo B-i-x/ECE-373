8df8fd828b27f9bfa902d45b409ef9f93b005ad5
B-i-x <bix.romero@gmail.com>
2023-08-29 09:34:29 -0700

    added rght testing

--- Lab1_RomeroLozano/test/edu/ece373/lab1/PersonTest.java
+++ Lab1_RomeroLozano/test/edu/ece373/lab1/PersonTest.java
@@ -1,5 +1,50 @@
 package edu.ece373.lab1;
 
-public class PersonTest {
-    
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import edu.ece373.lab1.Person;
+
+class PersonTest {
+
+	@Test
+	final void testPerson() {
+		Person p1 = new Person();
+		assertEquals("unknown", p1.getName());
+		assertEquals(2, p1.getMaxBooks());
+	}
+
+	@Test
+	final void testPersonStringInt() {
+		Person p2 = new Person("Joey", 500);
+		assertEquals("Joey", p2.getName());
+		assertEquals(500, p2.getMaxBooks());
+	}
+
+	@Test
+	final void testPersonString() {
+		Person p3 = new Person("Griselda Hoyer");
+		assertEquals("Griselda Hoyer", p3.getName());
+		assertEquals(0, p3.getMaxBooks());
+	}
+
+	@Test
+	final void testSetName() {
+		Person p1 = new Person();
+		assertEquals("unknown", p1.getName());
+		
+		p1.setName("Roy");
+		assertEquals("Roy", p1.getName());
+
+	}
+
+	@Test
+	final void testSetMaxBooks() {
+		Person p1 = new Person();
+		assertEquals(2, p1.getMaxBooks());
+		
+		p1.setMaxBooks(16777216);
+		assertEquals(16777216, p1.getMaxBooks());
+	}
+
 }
