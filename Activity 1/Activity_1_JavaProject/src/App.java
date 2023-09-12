1657a032a8f5acda8d641262b9946d32062c1f34
B-i-x <bix.romero@gmail.com>
2023-09-12 08:46:06 -0700

    one liner

--- Activity 1/Activity_1_JavaProject/src/App.java
+++ Activity 1/Activity_1_JavaProject/src/App.java
@@ -4,12 +4,10 @@ import worker.Employee;
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Date james_bond_hire_date = new Date(9, 17, 2023);
-
         Employee james_bond = new Employee(
             "James Bond", 
             100000, 
-            james_bond_hire_date
+            new Date(9, 17, 2023)
         );
 
         System.out.println(james_bond.printEmployee());
--- Activity 1/read_me.txt
+++ Activity 1/read_me.txt
@@ -1,2 +1,4 @@
 in the two text files are the uml
-in activity_1_JavaProject->src->worker are my date and employee files
+in activity_1_JavaProject->src->worker are my date and employee class
+in activity_1_JavaProject->src->App.java is where I test the classes
+I have also added a screenshot of the output
