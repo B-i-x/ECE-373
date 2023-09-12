d19c4a259d7506dc084259c7071d52436dc40e8a
B-i-x <bix.romero@gmail.com>
2023-09-12 08:42:11 -0700

    added whitespace

--- Activity 1/Activity_1_JavaProject/src/App.java
+++ Activity 1/Activity_1_JavaProject/src/App.java
@@ -3,7 +3,9 @@ import worker.Employee;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        
         Date james_bond_hire_date = new Date(9, 17, 2023);
+
         Employee james_bond = new Employee(
             "James Bond", 
             100000, 
